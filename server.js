340f2d1956c85241a44abf63f17289db611ced1c
Chris Hicks <chrhicks@gmail.com>
2015-12-13 10:50:28 -0500

    more build cleanup

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('*', (req, res) => {
+
   match({ routes, location: req.url }, (err, redirectLocation, props) => {
     if (err) {
       res.status(500).send(err.message);
